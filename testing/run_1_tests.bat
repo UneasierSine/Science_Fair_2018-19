a1f840ef5402c2581603c138a773d0fe1e4dbeae
UneasierSine <32441247+UneasierSine@users.noreply.github.com>
2019-01-26 09:59:40 -0500

    dfj j'i
    ;isvjw

--- testing/run_1_tests.bat
+++ testing/run_1_tests.bat
@@ -3,12 +3,7 @@
 cmd /C conda activate tf_gpu
 SET root=C:\Users\panka\Desktop\Science_Fair_2018-19\testing
 
-python "%root%\Parallel_Testing\run_1\1\Parallel_Testing_1.py"
-python "%root%\Parallel_Testing\run_2\1\Parallel_Testing_2.py"
-
 python "%root%\Normal_Testing\run_3\1\Normal_Testing_3.py"
-python "%root%\Parallel_Testing\run_3\1\Parallel_Testing_3.py"
 
 python "%root%\Feed_Testing\run_4\1\Feed_Testing_4.py"
 python "%root%\Normal_Testing\run_4\1\Normal_Testing_4.py"
-python "%root%\Parallel_Testing\run_4\1\Parallel_Testing_4.py"
