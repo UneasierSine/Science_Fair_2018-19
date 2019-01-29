fd059eff4fd70233a2c60715539db87ea18a310e
UneasierSine <32441247+UneasierSine@users.noreply.github.com>
2019-01-29 07:58:03 -0500

    feed run 2
    epic

---
+++ testing/Feed_Testing/run_1/files4n5.bat
@@ -0,0 +1,9 @@
+@echo off
+
+cd /D %cd%\4
+python Feed_Testing_1.py
+cd /D ..
+
+cd /D %cd%\5
+python Feed_Testing_1.py
+cd /D ..
---
+++ testing/Feed_Testing/run_3/run3_feed.bat
@@ -0,0 +1,21 @@
+@echo off
+
+cd /D %cd%\1
+python Feed_Testing_3.py
+cd /D ..
+
+cd /D %cd%\2
+python Feed_Testing_3.py
+cd /D ..
+
+cd /D %cd%\3
+python Feed_Testing_3.py
+cd /D ..
+
+cd /D %cd%\4
+python Feed_Testing_3.py
+cd /D ..
+
+cd /D %cd%\5
+python Feed_Testing_3.py
+cd /D ..
